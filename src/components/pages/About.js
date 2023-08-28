22a83211580c512c4abd9a5015fe2929e653194b
Persistent <agrlarbig@gmail.com>
2023-08-28 12:48:39 -0600

    cleaned uo some text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,9 +8,10 @@ function About() {
       <h1 id='h1Tag'>Hello!</h1>
   <main>
       <div class="aboutMe">
-      <p>A little about me!</p>
-      <p>Before I moved into Web development, I worked in IT for a few years. Doing help-desk and security focused work. And before that I was a baker! I ran a small bagel company in Fort Collins, CO. I did that for four years before moving into IT. I currently live in the Northern Colorado area with my Wife and 15 month old son. </p> 
-      <p>In my free time I like watching horror movies and spending time with my family! I also love game development and have started getting into map layouts and design. </p>
+      <p>A little about me...</p>
+      <p> Before I moving into web development, I worked in IT for a few years. Doing help-desk and security focused work and before that I was a baker. I ran a small bagel company in Fort Collins, CO for four years. I currently live in the Northern Colorado area with my wife and young son. </p>
+
+      <p>In my free time I like watching horror movies and spending time with my family. I also love game development and have started getting into map layouts and design. </p>
       <hr></hr>
       <img id='img-friends' src={headshot} alt='Picture of Alec'></img>
       </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,8 +9,9 @@ import allMyFriendsImage from '../assets/allmyfriends.jpg';
       <main>
         <div className="aboutMe">
           <p>
-          My name is Alec. I am new to web development but looking forward to learning all that I can! I am currently freelancing and looking for a full time position. I have a few projects that I have worked on and will be adding more as I go. I am currently working on a few things that I am excited to share with you all. And some that are very much a work in progress.
-          </p>
+         My name is Alec and I am new to web development but looking forward to learning all that I can. I am currently freelancing and looking for a full time position. I have a few projects that I am working on and will be adding more as I go of which I am excited to share with you all. Some are very much a work in progress so I always invite feedback. 
+         </p>
+          <p> In my day to day I am a stay at home dad to a 1.5 year old. I like to game, I used to have a bagel side hustle but hung up the apron and switched it for a career in tech. I can chat anything from toddler stories, to Dark Souls and the best way to knead dough. I hope to hear from you soon!</p>
           <hr></hr>
           <img id='img' src={allMyFriendsImage} alt='ghosts in the air'></img>
         </div>
