b21ba2af91f82740ae89fb5721d249c61516340b
Persistent <agrlarbig@gmail.com>
2023-08-28 12:35:02 -0600

    fixed some typos

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ import allMyFriendsImage from '../assets/allmyfriends.jpg';
       <main>
         <div className="aboutMe">
           <p>
-          My name is Alec! I a new to web development but looking forward to learning all that I can! I am currently freelancing and looking for a full time position. I have a few projects that I have worked on and will be adding more as I go. I am currently working on a few things that I am excited to share with you all! And some that are very much a work in progress.
+          My name is Alec. I am new to web development but looking forward to learning all that I can! I am currently freelancing and looking for a full time position. I have a few projects that I have worked on and will be adding more as I go. I am currently working on a few things that I am excited to share with you all. And some that are very much a work in progress.
           </p>
           <hr></hr>
           <img id='img' src={allMyFriendsImage} alt='ghosts in the air'></img>
